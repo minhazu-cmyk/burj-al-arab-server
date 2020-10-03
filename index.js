d12d2e06d5a1d0525c3cf86cba6ac75b431babda
minhazu-cmyk <minhajabedin341@gmail.com>
2020-10-03 20:11:16 +0000

    first commit

--- index.js
+++ index.js
@@ -4,22 +4,26 @@ const port = 5000
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const admin = require('firebase-admin');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
 
-const password = "NewArab78"
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dhmoa.mongodb.net/burjAlArab?retryWrites=true&w=majority`;
+
 
 
 const app = express()
 app.use(cors());
 app.use(bodyParser.json());
-var serviceAccount = require("./burj-al-arab-37f5d-firebase-adminsdk-fmx1v-55c317ea79.json");
+var serviceAccount = require("./configs/burj-al-arab-37f5d-firebase-adminsdk-fmx1v-55c317ea79.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://burj-al-arab-37f5d.firebaseio.com"
 });
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://arabian:NewArab78@cluster0.dhmoa.mongodb.net/burjAlArab?retryWrites=true&w=majority";
+
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
 client.connect(err => {
   const bookings = client.db("burjAlArab").collection("bookings");
@@ -41,18 +45,18 @@ app.get('/bookings', (req, res) => {
   const bearer = req.headers.authorization;
   if(bearer&& bearer.startsWith('Bearer ')){
     const idToken = bearer.split(' ')[1];
-    console.log({idToken});
+    
 
     admin.auth().verifyIdToken(idToken)
   .then(function(decodedToken) {
     
     const tokenEmail = decodedToken.email;
     const queryEmail = req.query.email;
-    console.log(tokenEmail, queryEmail);
+    
 
-    if(tokenEmail==req.query.email){ 
+    if(tokenEmail==queryEmail){ 
         
-  bookings.find({email:req.query.email})
+  bookings.find({email:queryEmail})
   .toArray((err,documents)=>{
     
     res.send(documents);
@@ -62,6 +66,10 @@ app.get('/bookings', (req, res) => {
   }).catch(function(error) {
     // Handle error
   });
+  }
+  
+  else{
+    res.status(401).send("un-authorized log in")
   }
 })
 })
